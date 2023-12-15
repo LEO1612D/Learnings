0b6de308ee1f8504d0cede1d83ec3e9b787a5396
Nikunj Prajapati <nikunjprajapati0016@gmail.com>
2023-12-15 18:29:05 +0530

    Image with content

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   // Set the response HTTP header with a status code of 200 (OK) and content type
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Type', 'text/html');
   
   // Simple text reading
 //   // Send a response back to the client
@@ -17,13 +17,31 @@ const server = http.createServer((req, res) => {
 
     // Read File
 
-    fs.readFile('./assets/sample_png.png', (err, data) => {
+    fs.readFile('./assets/sample_png.png', (err, imageData) => {
         if(err) {
             res.statusCode = 404;
             res.end("Image Not Found!!");
         }
         else {
-            res.end(data);
+
+            // Send Html Content
+                  // Sending HTML content with an image tag referencing the image file
+      const htmlContent = `
+      <html>
+        <body>
+          <h1>Hello, this is text</h1>
+          <img src="data:image/png;base64,${Buffer.from(imageData).toString('base64')}"/>
+        </body>
+      </html>
+    `;
+    res.end(htmlContent);
+
+
+
+
+
+            // Send file only ----
+            //res.end(imageData);
         }
     });
 
